d7b5904e834173f58e1ac0d461bae9eb30639dfc
Rogério Reis da Silva <rogreis@gmail.com>
2022-08-15 17:16:47 +0100

    Change in get files for UbReviewer

--- Objects/Book.cs
+++ Objects/Book.cs
@@ -16,8 +16,48 @@ namespace UbStandardObjects.Objects
 
         public Translation RightTranslation { get; set; }
 
-        public List<Translation> Translations { get; set; }
+        public List<Translation> Translations { get; set; } = null;
 
+        public List<Translation> ObservableTranslations
+        {
+            get
+            {
+                List<Translation> list = new List<Translation>();
+                list.AddRange(Translations);
+                return list;
+            }
+        }
+
+        /// <summary>
+        /// Inicialize the list of available translations
+        /// </summary>
+        /// <param name="dataFiles"></param>
+        /// <returns></returns>
+        protected bool InicializeTranslations(GetDataFiles dataFiles)
+        {
+            try
+            {
+                DataFiles = dataFiles;
+                if (Translations == null)
+                {
+                    Translations = DataFiles.GetTranslations();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string message = $"Could not initialize available translations. See log.";
+                StaticObjects.Logger.Error(message, ex);
+                StaticObjects.Logger.FatalError(message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Get a translation from the list
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Translation GetTranslation(short id)
         {
             Translation trans = Translations.Find(o => o.LanguageID == id);
@@ -31,27 +71,22 @@ namespace UbStandardObjects.Objects
         }
 
 
-        public List<Translation> ObservableTranslations
-        {
-            get
-            {
-                List<Translation> list = new List<Translation>();
-                list.AddRange(Translations);
-                return list;
-            }
-        }
 
-
-        public abstract void StoreAnnotations(TOC_Entry entry, List<UbAnnotationsStoreData> annotations);
-
-        public abstract void DeleteAnnotations(TOC_Entry entry);
-
+        /// <summary>
+        /// Inicialize book and 2 translations
+        /// </summary>
+        /// <param name="dataFiles"></param>
+        /// <param name="leftTranslationId"></param>
+        /// <param name="rightTranslationID"></param>
+        /// <returns></returns>
         public virtual bool Inicialize(GetDataFiles dataFiles, short leftTranslationId, short rightTranslationID)
         {
             try
             {
-                DataFiles = dataFiles;
-                Translations = DataFiles.GetTranslations();
+                if (!InicializeTranslations(dataFiles))
+                {
+                    return false;
+                }
                 LeftTranslation = DataFiles.GetTranslation(leftTranslationId);
                 if (!LeftTranslation.CheckData()) return false;
                 RightTranslation = DataFiles.GetTranslation(rightTranslationID);
@@ -60,12 +95,51 @@ namespace UbStandardObjects.Objects
             }
             catch (Exception ex)
             {
-                string message = $"General error getting translations: {ex.Message}. May be you do not have the correct data to use this tool.";
+                string message = $"Could not initialize translations {leftTranslationId} and {rightTranslationID}. See log.";
                 StaticObjects.Logger.Error(message, ex);
                 StaticObjects.Logger.FatalError(message);
                 return false;
             }
         }
+
+        /// <summary>
+        /// Inicialize only the book object
+        /// </summary>
+        /// <param name="dataFiles"></param>
+        /// <returns></returns>
+        public virtual bool Inicialize(GetDataFiles dataFiles)
+        {
+            return InicializeTranslations(dataFiles);
+        }
+
+        /// <summary>
+        /// Inicialize only the book object
+        /// </summary>
+        /// <param name="dataFiles"></param>
+        /// <returns></returns>
+        public virtual Translation InicializeTranslations(GetDataFiles dataFiles, short translationId)
+        {
+            try
+            {
+                if (!InicializeTranslations(dataFiles))
+                {
+                    return null;
+                }
+                return DataFiles.GetTranslation(translationId);
+            }
+            catch (Exception ex)
+            {
+                string message = $"Could not initialize translation {translationId}. See log.";
+                StaticObjects.Logger.Error(message, ex);
+                StaticObjects.Logger.FatalError(message);
+                return null;
+            }
+        }
+
+        public abstract void StoreAnnotations(TOC_Entry entry, List<UbAnnotationsStoreData> annotations);
+
+        public abstract void DeleteAnnotations(TOC_Entry entry);
+
 
 
     }
